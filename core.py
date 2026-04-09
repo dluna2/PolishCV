cd495d79473bb024632b218ef34ec538e44e3a54
dluna2 <dani.delvis.luna@gmail.com>
2026-04-08 23:23:29 -0700

    Was able to launch Streamlit app using commands on python terminal. app.py cannot reference code related to metrics, prompting, models, testing/training, nor other utitlity functions from the jupyter notebook. Since Streamlit requires logic to be written in a py file, we would have to experiment with code in the notebook, and then paste working code/functions into a separate py file for app.py to reference. If it's too complicated, we could use Gradio as a back up plan since it can deliver webapp through notebooks without needing to execute commands through python terminal. But I prefer Streamlit as it has better UI.

---
+++ core.py
@@ -0,0 +1,9 @@
+""" 
+For Streamlit, we need to have a core.py file that contains all the core functionality of the application. 
+This includes
+
+paste the working code here, and add any necessary imports, functions, classes, etc. from
+the resumecv.ipynb notebook. This file should be able to run without any errors, and should contain 
+all the code related to api calls to model, ai model functions, prompts, and any other core 
+functionality of the application.
+"""
